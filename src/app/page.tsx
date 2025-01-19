e15401289cdcff4c26607fccc266946839b0abd8
Tahabaig044 <maheenbaig330@gmail.com>
2025-01-19 12:20:09 +0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HomePage from "@/components/HomePage";
+import Test from "@/components/test";
 
 
 export default function Home() {
   return (
     <div>
+      <Test/>
       <HomePage/>
     </div>
   );
